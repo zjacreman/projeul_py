380ad8ee8ebc0e39b3533499e1ac96a7413d26cb
Zakk Acreman <zakk@acreman.net>
2015-08-12 12:00:10 -0500

    Edited p11 for clarity, added comments

--- projeul_p_11.py
+++ projeul_p_11.py
@@ -10,22 +10,23 @@ f.close()
 
 OFFSET = 3
 
-inc = lambda x,y: x + y
-dec = lambda x,y: x - y
-zero = lambda x,y: x
+inc = lambda x,y: x + y # Increments the row or column by up to OFFSET
+dec = lambda x,y: x - y # Decrements the row or column by up to OFFSET
+zero = lambda x,y: x    # Leaves row or column alone
 
-# Define helper function
-def prod(r, c, rfun, cfun):
+# Helper function that applies lambda functions to starting coordinates
+def prod(start_row, start_col, row_func, col_func):
     try:
         product = 1
         for add in range(OFFSET + 1):
-            product *= GRID[rfun(r, add)][cfun(c, add)]
+            product *= GRID[row_func(start_row, add)][col_func(start_col, add)]
         return product
-    except:
+    except: # ran up against a border, product invalid
         return 0
 
 max_product = 0
 
+# Start in upper left corner, move right then down
 for row_i in range(len(GRID)):
     for col_i in range(len(GRID)): # Assumes a square GRID
         max_product = max(prod(row_i, col_i, zero, inc), max_product)
